2718520c0598dfd1a8bbfbfad57f825854816e0a
Mariana <marianabmoraes.ef@gmail.com>
2024-06-11 11:11:52 -0300

    update commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let numero = "";
+let i = 1;
+var aleatorio = Math.floor(Math.random() * 101);
+
+console.log("Jogo da adivinhação!");
+console.log("Digite um número de 0 a 100 (Você tem 3 tentativas):");
+
+process.stdin.on("data", function (data) {
+  if (data.toString().trim().toLowerCase() == "não") {
+    console.log("Até mais!");
+    process.exit();
+  } else if (data.toString().trim().toLowerCase() == "sim") {
+    numero = "";
+    i = 1;
+    aleatorio = Math.floor(Math.random() * 101);
+    console.log("Digite o número:");
+  } else {
+    numero = Number(data.toString().trim());
+    if (i == 3 && numero != aleatorio) {
+      console.log("Você atingiu o número máximo de tentativas!");
+      console.log("O número correto é: " + aleatorio);
+      console.log("Deseja jogar novamente? (sim ou não)");
+    } else {
+      if (numero == aleatorio) {
+        console.log("Parabéns, você acertou! :)");
+        console.log("Deseja jogar novamente? (sim ou não)");
+      } else if (Math.abs(numero - aleatorio) < 5 * 0.3) {
+        console.log("Quente! Você está próximo.");
+        console.log("Você ainda tem " + (3 - i) + " tentativas.");
+        i++;
+      } else {
+        console.log("Frio! Você ainda tem " + (3 - i) + " tentativas.");
+        i++;
+      }
+    }
+  }
+});
